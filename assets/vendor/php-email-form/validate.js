aa64b69ac6e921eae608ad649c9a875a6f0ca83a
Shanto Miah <web.dev.shanto@gmail.com>
2021-08-19 15:34:33 +0600

    Add sopurt for contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -8,78 +8,82 @@
 
   let forms = document.querySelectorAll('.php-email-form');
 
-  forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
+  forms.forEach(function (e) {
+
+
+    e.addEventListener('submit', function (event) {
       event.preventDefault();
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+      let formData = new FormData(thisForm);
 
-      let formData = new FormData( thisForm );
+      let data = {}
+      formData.forEach((val, key) => data[key] = val)
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
+
+      sendMail(data, thisForm)
     });
+
+
   });
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
+  function sendMail(data, thisForm) {
+    displayLoader(thisForm);
+
+    fetch('https://portfolio-contact-server.herokuapp.com/mail/send', {
       method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json()).then(data => {
+      if (data.success) {
+        displaySucces(thisForm)
+        thisForm.reset()
       } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+        let error = ''
+        if (data.error.name) {
+          data.error.name.forEach(message => error += message + '</br>')
+        }
+
+        if (data.error.email) {
+          data.error.email.forEach(message => error += message + '</br>')
+        }
+
+        if (data.error.subject) {
+          data.error.subject.forEach(message => error += message + '</br>')
+        }
+
+        if (data.error.message) {
+          data.error.message.forEach(message => error += message + '</br>')
+        }
+
+        displayError(thisForm, error)
+
       }
+    }).catch(err => {
+      displayError(thisForm, err.message)
     })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
   }
+
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+  function displayLoader(thisForm) {
+    thisForm.querySelector('.loading').classList.add('d-block');
+    thisForm.querySelector('.error-message').classList.remove('d-block');
+    thisForm.querySelector('.sent-message').classList.remove('d-block');
+  }
+
+  function displaySucces(thisForm) {
+    thisForm.querySelector('.sent-message').classList.add('d-block')
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').classList.remove('d-block');
   }
 
 })();
